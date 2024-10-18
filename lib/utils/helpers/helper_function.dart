ec28fba279f9dba3301550175519f7f3a9fc32f3
Montalmo <photowedg@gmail.com>
2024-10-18 17:42:43 +0300

    add: helper functions

---
+++ lib/utils/helpers/helper_function.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
+
+class FZHelperFuctions {
+  static Color? getColor(String value) {
+    if (value == 'Green') {
+      return Colors.green;
+    } else if (value == 'Red') {
+      return Colors.red;
+    } else if (value == 'Blue') {
+      return Colors.blue;
+    } else if (value == 'Pink') {
+      return Colors.pink;
+    } else if (value == 'Grey') {
+      return Colors.grey;
+    } else if (value == 'Purple') {
+      return Colors.purple;
+    } else if (value == 'Black') {
+      return Colors.black;
+    } else if (value == 'White') {
+      return Colors.white;
+    } else if (value == 'Teal') {
+      return Colors.teal;
+    } else if (value == 'Indigo') {
+      return Colors.indigo;
+    } else {
+      return null;
+    }
+  }
+
+  static void showAlert(BuildContext context, String title, String message) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(title),
+            content: Text(message),
+            actions: [
+              TextButton(
+                onPressed: () => context.pop(),
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        });
+  }
+
+  static void navigateToScreen(BuildContext context, String location) {
+    context.pushNamed(location);
+  }
+
+  static String truncateString(String text, int maxLenght) {
+    if (text.length <= maxLenght) {
+      return text;
+    } else {
+      return '${text.substring(0, maxLenght)}...';
+    }
+  }
+
+  static bool isDarkMode(BuildContext context) {
+    return Theme.of(context).brightness == Brightness.dark;
+  }
+
+  static Size screenSize(BuildContext context) {
+    return MediaQuery.of(context).size;
+  }
+
+  static double screenHight(BuildContext context) {
+    return MediaQuery.of(context).size.height;
+  }
+
+  static double screenWidth(BuildContext context) {
+    return MediaQuery.of(context).size.width;
+  }
+
+  static String getFormatteddate(DateTime date,
+      {String format = 'dd MMM YYYY'}) {
+    return DateFormat(format).format(date);
+  }
+
+  static List<T> romoveDuplicates<T>(List<T> list) {
+    return list.toSet().toList();
+  }
+
+  static List<Widget> wrapWidgets(List<Widget> widgets, int rowSize) {
+    final wrapperList = <Widget>[];
+    for (var i = 0; i < widgets.length; i += rowSize) {
+      final rowChildren = widgets.sublist(
+          i, i + rowSize > widgets.length ? widgets.length : i + rowSize);
+      wrapperList.add(
+        Row(
+          children: rowChildren,
+        ),
+      );
+    }
+    return wrapperList;
+  }
+}
