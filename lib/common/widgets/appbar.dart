888e8741cd5dd2ebf963e936c029b39011eb7162
Montalmo <photowedg@gmail.com>
2024-11-11 15:53:00 +0200

    ref: appbar add always dark color to icon

--- lib/common/widgets/appbar.dart
+++ lib/common/widgets/appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fz_store/utils/constants/colors.dart';
 
 import 'package:fz_store/utils/device/device_utility.dart';
 import 'package:go_router/go_router.dart';
@@ -11,10 +12,12 @@ class FZAppBar extends StatelessWidget implements PreferredSizeWidget {
     this.actions,
     this.leadingPressed,
     this.showBackArrow = true,
+    this.alwaysDark = false,
   });
 
   final Widget? title;
   final bool showBackArrow;
+  final bool alwaysDark;
   final IconData? leadingIcon;
   final List<Widget>? actions;
   final VoidCallback? leadingPressed;
@@ -31,7 +34,9 @@ class FZAppBar extends StatelessWidget implements PreferredSizeWidget {
               onPressed: () {
                 context.pop();
               },
-              icon: const Icon(Icons.arrow_back),
+              icon: alwaysDark
+                  ? const Icon(Icons.arrow_back, color: FZColors.dark)
+                  : const Icon(Icons.arrow_back),
             )
           : leadingIcon != null
               ? IconButton(
